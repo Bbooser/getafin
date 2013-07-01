cccf9da25a45ade0ccadfecc1b6a6841b4cbdc9d
Pablo <booser92@gmail.com>
2013-07-01 11:44:52 -0500

    Create connect.js

---
+++ connect.js
@@ -0,0 +1,34 @@
+<script>
+ 
+$('#formulario').submit(function() { 
+  
+	 
+	// recolecta los valores que inserto el usuario
+	var datosUsuario = $("#nombredeusuario").val()
+	var datosPassword = $("#clave").val()
+	
+  	archivoValidacion = "http://getafin.net16.net/login.php?jsoncallback=?"
+ 
+	$.getJSON( archivoValidacion, { usuario:datosUsuario ,password:datosPassword})
+	.done(function(respuestaServer) {
+		
+		alert(respuestaServer.mensaje + "\nGenerado en: " + respuestaServer.hora + "\n" +respuestaServer.generador)
+		
+		if(respuestaServer.validacion == "ok"){
+		  
+		 	/// si la validacion es correcta, muestra la pantalla "home"
+			$.mobile.changePage("#home")
+		  
+		}else{
+		  
+		  alert("Usuario o Password Invalidos")
+		}
+  
+	})
+	return false;
+})
+	
+</script>
+ 
+</body>
+</html>
